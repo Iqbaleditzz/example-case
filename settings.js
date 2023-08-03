a72d831d12921cc3bd1440cca84c154b58ba6877
Iqbal <89727636+Iqbaleditzz@users.noreply.github.com>
2023-08-04 00:23:55 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -7,22 +7,22 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'wFxi498tajIQyxW',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
+global.namabot = "Rouxinoiz"
+global.namaowner = "iqbal K A"
 global.packname = namabot
 global.author = namaowner
 global.myweb = "https://s.id/zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['919567063916']
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
-global.grup_only = false //bot hanya bisa di gunakan dalam grup jika true
+global.grup_only = true //bots can only be used in groups if true
 global.mess = {
     done: 'Done ✅',
     wait: '_⌛ Sedang di proses_',
